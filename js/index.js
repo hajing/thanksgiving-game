9bb6fe7c170f0054ca0061fd80056461ec9bbe51
hesufan <hejinghemin@gmail.com>
2017-11-23 19:20:13 +0800

    优化结构

--- js/index.js
+++ js/index.js
@@ -6,9 +6,8 @@ $(function () {
         var keycode = event.which;
         if (keycode == 13) {
             var url_value = $("#inputinfo").val();
-            if(url_value == ""){
-                alert("请输入单词再访问呀");
-                return false;
+            if(url_value != 'standard'){
+                window.open('https://hajing.github.io/thanksgiving-game/success.html', "_blank");
             }else {
                 var url_open = 'https://hajing.github.io/thanksgiving-game/' + url_value + '.html';
                 window.open(url_open, "_blank");
